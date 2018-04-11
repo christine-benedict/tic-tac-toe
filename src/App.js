e3dc8a88d4896da07c0db1a8385d3ac202f16fdd
learn <learn@learn08.localdomain>
2018-04-11 13:24:22 -0700

    All cells functional. fixed repeat click issue.

--- src/App.js
+++ src/App.js
@@ -1,51 +1,55 @@
 import React, { Component } from 'react';
 import './App.css';
-import {Grid, Row, Col, Clearfix} from 'react-bootstrap'
+import {Grid, Row, Col} from 'react-bootstrap'
 class App extends Component {
     constructor(props){
         super(props)
         this.state = {
             topleft: '',
             topmid: '',
+            topright: '',
+            midleft: '',
+            midmid: '',
+            midright: '',
+            botleft: '',
+            botmid: '',
+            botright: '',
             userTurn: 'X',
         }
     }
 
     userMark(event){
         const attribute = event.target.id
-        this.setState({[attribute]: this.state.userTurn})
-    }
-
-    newPlayer(event){
-        console.log(this.state.userTurn);
-        if (this.state.userTurn === 'X'){
-            console.log("If userturn is X");
-        this.setState({userTurn: 'O'})
-        } else {
-        this.setState({userTurn: 'X'})
-
+        if( this.state[attribute] == '') {
+            this.setState({[attribute]: this.state.userTurn})
+            if (this.state.userTurn === 'X'){
+                this.setState({userTurn: 'O'})
+            } else {
+                this.setState({userTurn: 'X'})
+            }
         }
     }
+
 
     render() {
         return (
           <div className="App">
             <p> Tic-tac-toe </p>
-            <Grid onClick={this.newPlayer.bind(this)} className = "grid">
+            <Grid className = "grid">
                 <Row className="linedrow">
                     <Col onClick={this.userMark.bind(this)} className = "col" id="topleft" xs={4} md={4}>{this.state.topleft}</Col>
                     <Col onClick={this.userMark.bind(this)} className = "midcol" id="topmid" xs={4} md={4}> {this.state.topmid}</Col>
-                    <Col className = "col" id="topright" xs={4} md={4}> </Col>
+                    <Col onClick={this.userMark.bind(this)} className = "col" id="topright" xs={4} md={4}> {this.state.topright}</Col>
                 </Row>
                 <Row className="linedrow">
-                    <Col className = "col" id="topleft" xs={4} md={4}> </Col>
-                    <Col className = "midcol" id="topmid" xs={4} md={4}> </Col>
-                    <Col className = "col" id="topright" xs={4} md={4}> </Col>
+                    <Col onClick={this.userMark.bind(this)} className = "col" id="midleft" xs={4} md={4}>{this.state.midleft} </Col>
+                    <Col onClick={this.userMark.bind(this)} className = "midcol" id="midmid" xs={4} md={4}> {this.state.midmid} </Col>
+                    <Col onClick={this.userMark.bind(this)} className = "col" id="midright" xs={4} md={4}> {this.state.midright}</Col>
                 </Row>
                 <Row className="row">
-                    <Col className = "col" id="topleft" xs={4} md={4}> </Col>
-                    <Col className = "midcol" id="topmid" xs={4} md={4}> </Col>
-                    <Col className = "col" id="topright" xs={4} md={4}> </Col>
+                    <Col onClick={this.userMark.bind(this)} className = "col" id="botleft" xs={4} md={4}> {this.state.botleft}</Col>
+                    <Col onClick={this.userMark.bind(this)} className = "midcol" id="botmid" xs={4} md={4}> {this.state.botmid}</Col>
+                    <Col onClick={this.userMark.bind(this)} className = "col" id="botright" xs={4} md={4}>{this.state.botright} </Col>
                 </Row>
             </Grid>
 
