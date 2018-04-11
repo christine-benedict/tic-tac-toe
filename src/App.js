37fd6d169d10b45f7abe929ff0bbb305752adc3c
learn <learn@learn08.localdomain>
2018-04-11 11:44:19 -0700

    Got x to appear and changing from x to o

--- src/App.js
+++ src/App.js
@@ -2,26 +2,50 @@ import React, { Component } from 'react';
 import './App.css';
 import {Grid, Row, Col, Clearfix} from 'react-bootstrap'
 class App extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            topleft: '',
+            topmid: '',
+            userTurn: 'X',
+        }
+    }
+
+    userMark(event){
+        const attribute = event.target.id
+        this.setState({[attribute]: this.state.userTurn})
+    }
+
+    newPlayer(event){
+        console.log(this.state.userTurn);
+        if (this.state.userTurn === 'X'){
+            console.log("If userturn is X");
+        this.setState({userTurn: 'O'})
+        } else {
+        this.setState({userTurn: 'X'})
+
+        }
+    }
 
     render() {
         return (
           <div className="App">
             <p> Tic-tac-toe </p>
-            <Grid className = "grid">
+            <Grid onClick={this.newPlayer.bind(this)} className = "grid">
                 <Row className="linedrow">
-                    <Col className = "col" id="topleft" xs={6} md={4}> </Col>
-                    <Col className = "midcol" id="topmid" xs={6} md={4}> </Col>
-                    <Col className = "col" id="topright" xs={6} md={4}> </Col>
+                    <Col onClick={this.userMark.bind(this)} className = "col" id="topleft" xs={4} md={4}>{this.state.topleft}</Col>
+                    <Col onClick={this.userMark.bind(this)} className = "midcol" id="topmid" xs={4} md={4}> {this.state.topmid}</Col>
+                    <Col className = "col" id="topright" xs={4} md={4}> </Col>
                 </Row>
                 <Row className="linedrow">
-                    <Col className = "col" id="topleft" xs={6} md={4}> </Col>
-                    <Col className = "midcol" id="topmid" xs={6} md={4}> </Col>
-                    <Col className = "col" id="topright" xs={6} md={4}> </Col>
+                    <Col className = "col" id="topleft" xs={4} md={4}> </Col>
+                    <Col className = "midcol" id="topmid" xs={4} md={4}> </Col>
+                    <Col className = "col" id="topright" xs={4} md={4}> </Col>
                 </Row>
                 <Row className="row">
-                    <Col className = "col" id="topleft" xs={6} md={4}> </Col>
-                    <Col className = "midcol" id="topmid" xs={6} md={4}> </Col>
-                    <Col className = "col" id="topright" xs={6} md={4}> </Col>
+                    <Col className = "col" id="topleft" xs={4} md={4}> </Col>
+                    <Col className = "midcol" id="topmid" xs={4} md={4}> </Col>
+                    <Col className = "col" id="topright" xs={4} md={4}> </Col>
                 </Row>
             </Grid>
 
