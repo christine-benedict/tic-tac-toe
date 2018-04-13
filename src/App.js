68baee7c69e6db6536736db8cdfe46440acd4d57
learn <learn@learn08.localdomain>
2018-04-13 10:55:15 -0700

    fastpuzzler and I added game reset (initial state) and styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
 import Square from './components/square'
 import './App.css';
+
+var done = false
+
 class App extends Component {
     constructor(props){
         super(props)
         this.state = {
-            board: ['','','','','','','','','',],
+            board: ['','','','','','','','',''],
             userTurn: 'X',
             counter: 0
         }
     }
 
     userMark(element, position){
+        done = false
         let { board, userTurn, counter } = this.state
         if( element === '' ) {
             // update content of square
@@ -22,6 +26,11 @@ class App extends Component {
             // console.log(counter);
             // console.log(board[0]);
             this.winCondition(board)
+            if (done === true){
+                board = ['','','','','','','','','']
+                userTurn = 'X'
+                counter = 0
+            }
         }
         this.setState({
             board: board,
@@ -34,30 +43,25 @@ class App extends Component {
     winCondition(board){
         let winners = [ [0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6] ]
 
-
         for(let i=0; i<winners.length; i++){
             const [a,b,c] = winners[i]
-            console.log(i)
             if( board[a] !== '' && board[a] === board[b] && board[a] === board[c] ){
                 alert( board[a] + " wins!")
-                window.location.reload()
+                done = true
                 break
-            }else if ( i === 7 && !board.includes('')){
+            } else if ( i === 7 && !board.includes('')){
                 alert("No Winner :(.. try again..")
-                window.location.reload()
+                done = true
             }
         }
-
-
-
-        }
-
+        return done
+    }
 
     render(){
         let { board } = this.state
         let squares = board.map( (element,index)=>{
             return(
-                <Square xOrO={element} clicky = {this.userMark.bind(this, element, index)}/>
+                <Square key={index} xOrO={element} clicky = {this.userMark.bind(this, element, index)}/>
             )
         })
 
@@ -69,5 +73,7 @@ class App extends Component {
         )
     }
 }
+
+
 
 export default App;
